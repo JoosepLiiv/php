e30cc396276f41b885e17e69fd42b959e46253ba
JoosepLiiv <joosepliiv97@gmail.com>
2017-01-11 14:43:25 +0200

    text class initial version

---
+++ ob/text.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Joosep
+ * Date: 11.01.2017
+ * Time: 14:23
+ */
+class text
+{
+    //text class begin
+        var $str = '';
+
+        function setText($s){
+            $this->str = $s;
+        }
+
+        function show(){
+            echo $this->str.'<br>';
+        }
+    //text class end
+}
