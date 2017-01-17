bfb14a6b5c8e9141dd329df344bee5b9f8a7a2bb
Joosep <joosepliiv97@gmail.com>
2017-01-17 13:11:01 +0200

    Create http class initial version

---
+++ classes/http.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Joosep
+ * Date: 17.01.2017
+ * Time: 12:51
+ */
+class http
+{
+    var $server = array(); // server data
+    var $vars = array(); // http data
+    var $cookies = array(); //cookies data
+
+    function __construct()
+    {
+        $this->init();
+        $this->initConst();
+    }
+
+    // initializing
+    function init(){
+        $this->server = $_SERVER;
+        $this->vars = $_COOKIE;
+        $this->cookies = array_merge($_GET,$_POST,$_FILES);
+    }
+
+    function initConst(){
+        $vars = array('REMOTE_ADDR','PHP_SELF','SCRIPT_NAME','HTTP_HOST');
+        foreach ($vars as $var){
+            if(!defined($var) and isset($this->server[$var])){
+                define($var, $this->server[$var]);
+            }
+        }
+    }
+}
