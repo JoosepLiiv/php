64ec874bcc6e3c12392b699e68ef121a4c48438a
Joosep <joosepliiv97@gmail.com>
2017-01-30 12:42:36 +0200

    login_do act

---
+++ acts/login_do.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joosep
+ * Date: 30.01.2017
+ * Time: 12:35
+ */
+$username = $http->get('username');
+$password = $http->get('password');
+
+$sql = 'SELECT * FROM user WHERE '.
+    'username='.fixDb($username).' AND '.
+    'password='.fixDb(md5($password)).' AND '.
+    'is_activated=1';
+$res = $db->getArray($sql);
+
+if($res === false)
+{
+    $sess->set('login_error', tr('Viga sisselogimisel'));
+
+    $link = $http->getLink(array('act' => 'login'), array('username'));
+    $http->redirect($link);
+}
+else
+{
+    $sess->createSession($res[0]);
+}
